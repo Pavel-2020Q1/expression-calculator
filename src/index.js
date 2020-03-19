eabb6572731f27489f106debf045a7299782879f
Pavel-2020Q1 <p.zabavskiy@tut.by>
2020-03-19 09:54:29 +0200

    init:add task expression-calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let count = 0;
+    expr.replace(/[\s]{2}/g, ' ').trim().split(' ')
+        .join('').split('').map(a => a == ')' ? count++ : a == '(' ? count-- : false);
+    if (count != 0) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+
+    for (let i = 0; i < expr.split('').length; i++) {
+        if (expr.split('')[i] === "/") {
+            if (expr.split('')[i + 2] === "0") {
+                throw new Error("TypeError: Division by zero.");
+            }
+        }
+    }
+    let result = new Function("return " + expr.replace(/[\s]/g, ''));
+
+    return result();
 }
 
 module.exports = {
